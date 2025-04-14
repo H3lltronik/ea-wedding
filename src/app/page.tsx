97ce4a823b3bcf44d614327fec2c6429ea0d025a
H3lltronik <esau.egs1@gmail.com>
2025-04-13 22:01:38 -0600

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import WeddingLogo from "./icons/WeddingLogo";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <WeddingLogo className="w-[600px] h-[600px] text-red-300" />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
